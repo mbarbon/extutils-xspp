f1d4582ef4f3b1096592bc02f492021688865cc8
Mattia Barbon <mattia.barbon@libero.it>
2009-09-20 14:28:20 +0200

    Document ExtUtils::XSpp::Cmd.

--- lib/ExtUtils/XSpp/Cmd.pm
+++ lib/ExtUtils/XSpp/Cmd.pm
@@ -2,6 +2,28 @@ package ExtUtils::XSpp::Cmd;
 
 use strict;
 
+=head1 NAME
+
+ExtUtils::XSpp::Cmd - implementation of xspp
+
+=head1 SYNOPSIS
+
+  perl -MExtUtils::XSpp::Cmd -e xspp -- <xspp options and arguments>
+
+In Foo.xs
+
+  INCLUDE: perl -MExtUtils::XSpp::Cmd -e xspp -- <xspp options and arguments>
+
+Using C<ExtUtils::XSpp::Cmd> is equivalent to using the C<xspp>
+command line script, except that there is no guarantee for C<xspp> to
+be installed in the system PATH.
+
+=head1 DOCUMENTATION
+
+See L<ExtUtils::XSpp>, L<xspp>.
+
+=cut
+
 use Exporter 'import';
 use Getopt::Long;
 
